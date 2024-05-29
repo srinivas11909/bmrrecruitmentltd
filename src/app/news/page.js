dd936174af923c573057a4d51a9cae35432df931
srinivas11909 <srinivas.reddy865@gmail.com>
2024-05-29 23:40:58 +0530

    seo added

--- src/app/news/page.js
+++ src/app/news/page.js
@@ -2,7 +2,7 @@ export default function News() {
     return <>    
             <div className="min-h-screen flex items-center">
                 <div className="container mx-auto flex items-center justify-center text-center">
-                     <h2 className="text-5xl">News Page</h2>
+                     <h2 className="text-5xl" style={{color: "#000000"}}>News Page</h2>
                 </div>
             </div>
     </>
